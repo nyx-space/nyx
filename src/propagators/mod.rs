331015b03fae1a8d914e463ddb950e9530c8897e
Christopher Rabotin <christopher.rabotin@gmail.com>
2018-02-12 01:10:28 -0700

    Drastically changed paradigm after thinking about similarities
    Needs thorough testing. Also needs the definition of the RKs as Butcher
    tables. But the good news now is that `Propagator` is the main struct to
    handle any RK which is consistent.
    
    TODO: Adaptive step size; Massive amounts of documentation (which will
    come after testing); Massive benchmarking to ensure that this code is
    both robust and especially fast.

--- src/propagators/mod.rs
+++ src/propagators/mod.rs
@@ -1,23 +1,93 @@
 extern crate nalgebra as na;
 
-use self::na::DVector;
+use self::na::{DMatrix, DVector};
+
+/// The `RK` trait defines a Runge Kutta
+pub trait RK
+where
+    Self: Sized,
+{
+    /// Returns a Matrix corresponding to the A coefficients of the Butcher table for that RK.
+    /// *Warning:* this RK trait supposes that the implementation is consistent, i.e. c_i = \sum_j a_{ij}.
+    fn a_coeffs() -> DMatrix<f64>;
+    /// Returns a Matrix corresponding to the b_i and b^*_i coefficients of the Butcher table for that RK
+    fn b_coeffs() -> DMatrix<f64>;
+    fn from_options(opts: Options) -> Self;
+    fn options(&self) -> Options;
+}
+
+#[derive(Clone)]
+pub struct Propagator {
+    ti: f64,
+    state: DVector<f64>,
+    opts: Options,
+    latest_error: f64,
+    a_coeffs: DMatrix<f64>,
+    b_coeffs: DMatrix<f64>,
+}
 
 /// The `Propagator` trait defines the functions of a propagator.
-pub trait Propagator {
+/// TODO: Add examples
+impl Propagator {
     /// Each propagator must be initialized with `new` in order to store the options needed for this propagation.
-    fn new(options: Options) -> Self;
+    pub fn new<T: RK>(t0: f64, state: DVector<f64>, method: T) -> Propagator {
+        Propagator {
+            ti: t0,
+            state: state,
+            opts: method.options(),
+            latest_error: 0.0,
+            a_coeffs: T::a_coeffs(),
+            b_coeffs: T::b_coeffs(),
+        }
+    }
+
     /// The `prop` method is monomorphic to increase speed. The `d_xdt` parameter is the derivative function which take
     /// a time t of type f64 and a state of type DVector<f64>, and returns the result as DVector<f64> of the derivative.
-    fn prop<F>(self, d_xdt: F) -> DVector<f64>
+    pub fn prop<F>(&mut self, d_xdt: F) -> DVector<f64>
     where
-        F: Fn(f64, DVector<f64>) -> DVector<f64>;
+        F: Fn(f64, DVector<f64>) -> DVector<f64>,
+    {
+        let mut k = Vec::new(); // Will store all the k_i.
+        let (order, _) = self.a_coeffs.shape();
+        for i in 0..order {
+            let ci: f64 = self.a_coeffs.row(i).iter().sum();
+            let mut wi = DVector::from_element(self.state.shape().0 as usize, 0.0);
+            for j in 0..k.len() {
+                let a_ij = self.a_coeffs.slice((i, j), (1, 1));
+                wi += a_ij * &k[j];
+            }
+            let ki = d_xdt(
+                self.ti + ci * self.opts.min_step,
+                self.state.clone() + self.opts.min_step * wi,
+            );
+            k.push(ki);
+        }
+        // Compute the next state and the error
+        let mut next_state = self.state.clone();
+        let mut next_state_star = self.state.clone();
+        for i in 0..k.len() {
+            let b_ij = self.a_coeffs.slice((i, 0), (1, 1));
+            let b_ij_star = self.a_coeffs.slice((i, 1), (1, 1));
+            next_state += self.opts.min_step * b_ij * &k[i];
+            next_state_star += self.opts.min_step * b_ij_star * &k[i];
+        }
+        // TODO: Adaptive step size
+        self.latest_error = (next_state.clone() - next_state_star).norm();
+        next_state
+    }
+    pub fn latest_error(self) -> f64 {
+        self.latest_error
+    }
+    pub fn latest_state(self) -> DVector<f64> {
+        self.state
+    }
 }
 
 /// Options stores the integrator options, including the minimum and maximum step sizes, and the max error size.
 /// Note that different step sizes and max errors are only used for adaptive methods. To use a fixed step
 /// integrator, initialize the options using `with_fixed_step`, and use whichever adaptive step integrator is desired.
 /// For example, initializing an RK45 with fixed step options will lead to an RK4 being used instead of an RK45.
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Options {
     min_step: f64,
     max_step: f64,
