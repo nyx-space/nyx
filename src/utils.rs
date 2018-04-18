b4d37746e7ab886900fd508dcf075c2a472b0acf
Christopher Rabotin <christopher.rabotin@gmail.com>
2018-04-17 22:05:37 -0600

    Fixed is_diagonal (it has an extra loop which was not broken asap).

--- src/utils.rs
+++ src/utils.rs
@@ -19,7 +19,6 @@ pub fn tilde_matrix(v: &Vector3<f64>) -> Matrix3<f64> {
 
 /// Returns whether the provided square matrix (3x3) is diagonal
 pub fn is_diagonal(m: &Matrix3<f64>) -> bool {
-    let mut is_diag = true;
     for i in 1..2 {
         for j in 0..i {
             if (i == j && (m[(i, j)] - m[(0, 0)]) > f64::EPSILON)
@@ -27,10 +26,9 @@ pub fn is_diagonal(m: &Matrix3<f64>) -> bool {
                     && (m[(i, j)].abs() > f64::EPSILON
                         || (m[(i, j)] - m[(j, i)]).abs() > f64::EPSILON))
             {
-                is_diag = false;
-                break;
+                return false;
             }
         }
     }
-    is_diag
+    true
 }
