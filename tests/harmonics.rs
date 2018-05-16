471d289ae5050e6ee533f1a730070a35dcd4223d
Christopher Rabotin <christopher.rabotin@gmail.com>
2018-05-16 09:03:31 -0600

    Was it simply a sign error? Result is now very close to expectation
    TF?! I'm sure I verified a half a billion times that there was no sign
    issue in the return?!
    
    Difference is still higher than what it should be, but at the meter
    level:
    thread 'two_body_j2_state_parametrized' panicked at 'assertion failed:
    `(left == right)`
      left: `State { gm: 398600.4415, x: -5751.473991026703, y:
    4721.214036506542, z: 2045.947766308757, vx: -0.797740263736765, vy:
    -3.656451982107864, vz: 6.139637922282735 }`,
     right: `State { gm: 398600.4415, x: -5751.474391385416, y:
    4721.174127635078, z: 2046.0363725252087, vx: -0.7976771619884576, vy:
    -3.6565093281152503, vz: 6.139612324840211 }`: two body prop failed
    expected: sma = 7724.723489 km  ecc = 0.002263  inc = 63.457202 deg
    raan = 132.724289 deg   aop = 81.868688 deg     ta = 64.630165 deg
    computed: sma = 7724.722891 km  ecc = 0.002263  inc = 63.457199 deg
    raan = 132.724157 deg   aop = 81.868547 deg     ta = 64.629251 deg'

--- tests/harmonics.rs
+++ tests/harmonics.rs
@@ -42,7 +42,7 @@ fn two_body_j2_state_parametrized() {
             _t: f64,
             state: &VectorN<f64, Self::StateSize>,
         ) -> VectorN<f64, Self::StateSize> {
-            self.twobody.eom(_t, state) + self.harmonics.eom(_t, state)
+            self.twobody.eom(_t, state) - self.harmonics.eom(_t, state)
         }
     }
 
