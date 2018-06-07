f7bd55d05810b4a2690c74c79f301d9d69fede8f
Christopher Rabotin <christopher.rabotin@gmail.com>
2018-06-06 20:29:53 -0600

    Upgrade to hifitime 0.1.4; Coded up TwoBodyWithStm, testing time!

--- src/dynamics/celestial.rs
+++ src/dynamics/celestial.rs
@@ -8,9 +8,9 @@ use std::f64;
 /// `TwoBody` exposes the equations of motion for a simple two body propagation.
 #[derive(Copy, Clone)]
 pub struct TwoBody {
+    pub mu: f64,
     time: f64,
     pos_vel: Vector6<f64>,
-    mu: f64,
 }
 
 impl TwoBody {
@@ -60,44 +60,16 @@ impl Dynamics for TwoBody {
 /// the State Transition Matrix for orbital determination.
 #[derive(Copy, Clone)]
 pub struct TwoBodyWithStm {
-    pub compute_stm: bool,
-    pub pos_vel: Vector6<f64>,
     pub stm: Matrix6<f64>,
-    time: f64,
-    mu: f64,
+    pub two_body_dyn: TwoBody,
 }
 
 impl TwoBodyWithStm {
-    /// Initialize TwoBody dynamics given a provided gravitional parameter (as `mu`)
-    pub fn from_state_vec_with_gm(state: &Vector6<f64>, mu: f64) -> TwoBodyWithStm {
-        TwoBodyWithStm {
-            compute_stm: false,
-            time: 0.0,
-            pos_vel: *state,
-            mu: mu,
-            stm: Matrix6::identity(),
-        }
-    }
-
-    /// Initialize TwoBody dynamics around a provided `CelestialBody` from the provided state vector (cf. nyx::celestia).
-    pub fn from_state_vec<B: CelestialBody>(state: &Vector6<f64>) -> TwoBodyWithStm {
-        TwoBodyWithStm {
-            compute_stm: false,
-            time: 0.0,
-            pos_vel: *state,
-            mu: B::gm(),
-            stm: Matrix6::identity(),
-        }
-    }
-
     /// Initialize TwoBody dynamics around a provided `CelestialBody` from the provided position and velocity state (cf. nyx::celestia).
     pub fn from_state<B: CelestialBody, F: CoordinateFrame>(state: State<F>) -> TwoBodyWithStm {
         TwoBodyWithStm {
-            compute_stm: false,
-            time: 0.0,
-            pos_vel: state.to_cartesian_vec(),
-            mu: B::gm(),
             stm: Matrix6::identity(),
+            two_body_dyn: TwoBody::from_state_vec::<B>(&state.to_cartesian_vec()),
         }
     }
 }
@@ -105,31 +77,34 @@ impl TwoBodyWithStm {
 impl Dynamics for TwoBodyWithStm {
     type StateSize = U42;
     fn time(&self) -> f64 {
-        self.time
+        self.two_body_dyn.time()
     }
 
     fn state(&self) -> VectorN<f64, Self::StateSize> {
         let stm_as_vec = self.stm.fixed_resize::<U1, U36>(0.0);
-        VectorN::<f64, Self::StateSize>::from_iterator(self.pos_vel.iter().chain(stm_as_vec.iter()).cloned())
+        VectorN::<f64, Self::StateSize>::from_iterator(self.two_body_dyn.state().iter().chain(stm_as_vec.iter()).cloned())
     }
 
     fn set_state(&mut self, new_t: f64, new_state: &VectorN<f64, Self::StateSize>) {
-        self.time = new_t;
-        self.pos_vel = new_state.fixed_rows::<U6>(0).into_owned();
+        self.two_body_dyn
+            .set_state(new_t, &new_state.fixed_rows::<U6>(0).into_owned());
+        self.stm = new_state.fixed_rows::<U36>(6).fixed_resize::<U6, U6>(0.0);
     }
 
-    fn eom(&self, _t: f64, state: &VectorN<f64, Self::StateSize>) -> VectorN<f64, Self::StateSize> {
-        let radius = state.fixed_rows::<U3>(0).into_owned();
-        let velocity = state.fixed_rows::<U3>(3).into_owned();
-        let body_acceleration = (-self.mu / radius.norm().powi(3)) * radius;
-        VectorN::<f64, Self::StateSize>::from_iterator(velocity.iter().chain(body_acceleration.iter()).cloned())
+    fn eom(&self, t: f64, state: &VectorN<f64, Self::StateSize>) -> VectorN<f64, Self::StateSize> {
+        let pos_vel = state.fixed_rows::<U6>(0).into_owned();
+        let two_body_dt = self.two_body_dyn.eom(t, &pos_vel);
+        let stm_dt = self.stm * self.gradient(t, &pos_vel);
+        VectorN::<f64, Self::StateSize>::from_iterator(
+            two_body_dt.iter().chain(stm_dt.fixed_resize::<U1, U36>(0.0).iter()).cloned(),
+        )
     }
 }
 
 impl Linearization for TwoBodyWithStm {
     type StateSize = U6;
 
-    fn gradient(&self, t: f64, state: &VectorN<f64, Self::StateSize>) -> MatrixMN<f64, Self::StateSize, Self::StateSize> {
+    fn gradient(&self, _t: f64, state: &VectorN<f64, Self::StateSize>) -> MatrixMN<f64, Self::StateSize, Self::StateSize> {
         let mut grad = MatrixMN::<f64, U6, U6>::zeros();
         // Top right is Identity 3x3
         grad[(3, 0)] = 1.0;
@@ -142,33 +117,30 @@ impl Linearization for TwoBodyWithStm {
         let x2 = x.powi(2);
         let y2 = y.powi(2);
         let z2 = z.powi(2);
-        let vx = state[(3, 0)];
-        let vy = state[(4, 0)];
-        let vz = state[(5, 0)];
         let r232 = (x2 + y2 + z2).powf(3.0 / 2.0);
         let r252 = (x2 + y2 + z2).powf(5.0 / 2.0);
 
         // Add the body perturbations
-        let dAxDx = 3.0 * self.mu * x2 / r252 - self.mu / r232;
-        let dAxDy = 3.0 * self.mu * x * y / r252;
-        let dAxDz = 3.0 * self.mu * x * z / r252;
-        let dAyDx = 3.0 * self.mu * x * y / r252;
-        let dAyDy = 3.0 * self.mu * y2 / r252 - self.mu / r232;
-        let dAyDz = 3.0 * self.mu * y * z / r252;
-        let dAzDx = 3.0 * self.mu * x * z / r252;
-        let dAzDy = 3.0 * self.mu * y * z / r252;
-        let dAzDz = 3.0 * self.mu * z2 / r252 - self.mu / r232;
+        let dax_dx = 3.0 * self.two_body_dyn.mu * x2 / r252 - self.two_body_dyn.mu / r232;
+        let dax_dy = 3.0 * self.two_body_dyn.mu * x * y / r252;
+        let dax_dz = 3.0 * self.two_body_dyn.mu * x * z / r252;
+        let day_dx = 3.0 * self.two_body_dyn.mu * x * y / r252;
+        let day_dy = 3.0 * self.two_body_dyn.mu * y2 / r252 - self.two_body_dyn.mu / r232;
+        let day_dz = 3.0 * self.two_body_dyn.mu * y * z / r252;
+        let daz_dx = 3.0 * self.two_body_dyn.mu * x * z / r252;
+        let daz_dy = 3.0 * self.two_body_dyn.mu * y * z / r252;
+        let daz_dz = 3.0 * self.two_body_dyn.mu * z2 / r252 - self.two_body_dyn.mu / r232;
 
         // Let the gradient
-        grad[(0, 3)] = dAxDx;
-        grad[(0, 4)] = dAyDx;
-        grad[(0, 5)] = dAzDx;
-        grad[(1, 3)] = dAxDy;
-        grad[(1, 4)] = dAyDy;
-        grad[(1, 5)] = dAzDy;
-        grad[(2, 3)] = dAxDz;
-        grad[(2, 4)] = dAyDz;
-        grad[(2, 5)] = dAzDz;
+        grad[(0, 3)] = dax_dx;
+        grad[(0, 4)] = day_dx;
+        grad[(0, 5)] = daz_dx;
+        grad[(1, 3)] = dax_dy;
+        grad[(1, 4)] = day_dy;
+        grad[(1, 5)] = daz_dy;
+        grad[(2, 3)] = dax_dz;
+        grad[(2, 4)] = day_dz;
+        grad[(2, 5)] = daz_dz;
 
         grad
     }
