153609bb19de622980d04a42aec1391df2950a55
Christopher Rabotin <rabotin@advanced-space.com>
2019-09-03 14:30:32 -0600

    Started working on #73

--- src/od/kalman.rs
+++ src/od/kalman.rs
@@ -2,8 +2,7 @@ extern crate nalgebra as na;
 
 use self::na::allocator::Allocator;
 use self::na::{DefaultAllocator, DimName, MatrixMN, VectorN};
-use super::serde::ser::SerializeSeq;
-use super::serde::{Serialize, Serializer};
+use super::estimate::Estimate;
 use std::fmt;
 
 /// Defines both a Classical and an Extended Kalman filter (CKF and EKF)
@@ -140,92 +139,6 @@ where
         self.h_tilde_updated = false;
         self.prev_estimate = estimate.clone();
         Ok(estimate)
-    }
-}
-
-/// Stores an Estimate, as the result of a `time_update` or `measurement_update`.
-#[derive(Debug, Clone, PartialEq)]
-pub struct Estimate<S>
-where
-    S: DimName,
-    DefaultAllocator: Allocator<f64, S> + Allocator<f64, S, S>,
-{
-    /// The estimated state
-    pub state: VectorN<f64, S>,
-    /// The Covariance of this estimate
-    pub covar: MatrixMN<f64, S, S>,
-    /// Whether or not this is a predicted estimate from a time update, or an estimate from a measurement
-    pub predicted: bool,
-    /// The STM used to compute this Estimate
-    pub stm: MatrixMN<f64, S, S>,
-}
-
-impl<S> Estimate<S>
-where
-    S: DimName,
-    DefaultAllocator: Allocator<f64, S> + Allocator<f64, S, S>,
-{
-    /// An empty estimate. This is useful if wanting to store an estimate outside the scope of a filtering loop.
-    pub fn empty() -> Estimate<S> {
-        Estimate {
-            state: VectorN::<f64, S>::zeros(),
-            covar: MatrixMN::<f64, S, S>::zeros(),
-            predicted: true,
-            stm: MatrixMN::<f64, S, S>::zeros(),
-        }
-    }
-
-    pub fn header() -> Vec<String> {
-        let mut hdr_v = Vec::with_capacity(3 * S::dim());
-        for i in 0..S::dim() {
-            hdr_v.push(format!("state_{}", i));
-        }
-        // Serialize the covariance
-        for i in 0..S::dim() {
-            for j in 0..S::dim() {
-                hdr_v.push(format!("covar_{}_{}", i, j));
-            }
-        }
-        hdr_v
-    }
-}
-
-impl<S> fmt::Display for Estimate<S>
-where
-    S: DimName,
-    DefaultAllocator: Allocator<f64, S> + Allocator<f64, S, S> + Allocator<usize, S> + Allocator<usize, S, S>,
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "=== PREDICTED: {} ===\nEstState {} Covariance {}\n=====================",
-            &self.predicted, &self.state, &self.covar
-        )
-    }
-}
-
-impl<S> Serialize for Estimate<S>
-where
-    S: DimName,
-    DefaultAllocator: Allocator<f64, S> + Allocator<f64, S, S> + Allocator<usize, S> + Allocator<usize, S, S>,
-{
-    /// Serializes the estimate
-    fn serialize<O>(&self, serializer: O) -> Result<O::Ok, O::Error>
-    where
-        O: Serializer,
-    {
-        let mut seq = serializer.serialize_seq(Some(S::dim() * 3))?;
-        // Serialize the state
-        for i in 0..S::dim() {
-            seq.serialize_element(&self.state[(i, 0)])?;
-        }
-        // Serialize the covariance
-        for i in 0..S::dim() {
-            for j in 0..S::dim() {
-                seq.serialize_element(&self.covar[(i, j)])?;
-            }
-        }
-        seq.end()
     }
 }
 
