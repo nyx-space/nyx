381ab67900e7c4c55320eec25aa420489788419d
Christopher Rabotin <christopher.rabotin@gmail.com>
2019-10-30 20:57:00 -0600

    Add eclipse test and clean up README
    Signed-off-by: Christopher Rabotin <christopher.rabotin@gmail.com>

--- tests/eclipse.rs
+++ tests/eclipse.rs
@@ -8,7 +8,6 @@ use hifitime::{Epoch, SECONDS_PER_DAY};
 use nyx::celestia::eclipse::{eclipse_state, EclipseState};
 use nyx::celestia::{bodies, Cosm, Geoid, State};
 use nyx::dynamics::celestial::CelestialDynamics;
-use nyx::propagators::error_ctrl::RSSStatePV;
 use nyx::propagators::{PropOpts, Propagator, RK89};
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
@@ -16,11 +15,10 @@ use std::thread;
 
 #[test]
 fn leo_moon_eclipses() {
-    let prop_time = 7.0 * SECONDS_PER_DAY;
+    let prop_time = 2.0 * SECONDS_PER_DAY;
 
     let cosm = Cosm::from_xb("./de438s");
     let earth = cosm.geoid_from_id(bodies::EARTH);
-    let luna = cosm.geoid_from_id(bodies::EARTH_MOON);
 
     let start_time = Epoch::from_gregorian_tai_at_midnight(2020, 1, 1);
 
@@ -33,24 +31,28 @@ fn leo_moon_eclipses() {
     thread::spawn(move || {
         let cosm = Cosm::from_xb("./de438s");
         let mut dynamics = CelestialDynamics::new(leo, bodies, &cosm);
-        let mut prop = Propagator::new::<RK89>(&mut dynamics, &PropOpts::with_fixed_step(10.0));
+        let mut prop = Propagator::new::<RK89>(&mut dynamics, &PropOpts::with_fixed_step(60.0));
         prop.tx_chan = Some(&truth_tx);
         prop.until_time_elapsed(prop_time);
     });
 
     // Receive the states on the main thread.
     let mut prev_eclipse_state = EclipseState::Umbra;
+    let mut cnt_changes = 0;
     while let Ok(rx_state) = truth_rx.recv() {
         let jde = rx_state.dt.as_jde_et_days();
         let moon_state = cosm.celestial_state(bodies::EARTH_MOON, jde, rx_state.frame.id);
-        let new_eclipse_state = eclipse_state(&rx_state, &moon_state, luna, &cosm);
+        let new_eclipse_state = eclipse_state(&rx_state, &moon_state, earth, &cosm);
         if new_eclipse_state != prev_eclipse_state {
             println!(
-                "{} now in {:?}",
+                "{:.6} now in {:?}",
                 rx_state.dt.as_jde_tai_days(),
                 new_eclipse_state
             );
             prev_eclipse_state = new_eclipse_state;
+            cnt_changes += 1;
         }
     }
+
+    assert_eq!(cnt_changes, 225, "wrong number of eclipse state changes");
 }
