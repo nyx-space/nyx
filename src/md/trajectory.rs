f9ee209ef013ecc4bbbdd69b27103f29d4bd0eed
Christopher Rabotin <christopher.rabotin@gmail.com>
2021-02-09 01:49:04 -0700

    Fixed bug in last window of trajectory
    Found bug in first window for spacecraft interp
    
    Signed-off-by: Christopher Rabotin <christopher.rabotin@gmail.com>

--- src/md/trajectory.rs
+++ src/md/trajectory.rs
@@ -9,6 +9,10 @@ use std::collections::BTreeMap;
 use std::sync::mpsc::Receiver;
 use std::time::Duration as StdDur;
 
+const ITEMS_PER_SEGMENTS: usize = 32;
+const INTERP_TOLERANCE: f64 = 1e-10;
+
+/// Stores a segment of an interpolation
 pub struct Segment<S: State>
 where
     DefaultAllocator: Allocator<f64, S::PropVecSize> + Allocator<f64, S::Size>,
@@ -16,10 +20,10 @@ where
     start_epoch: Epoch,
     duration: Duration,
     coefficients: Vec<Vec<f64>>,
-    start_state: S,
     end_state: S,
 }
 
+/// Store a trajectory of any State.
 pub struct Traj<S: State>
 where
     DefaultAllocator: Allocator<f64, S::PropVecSize> + Allocator<f64, S::Size>,
@@ -27,30 +31,30 @@ where
     /// Segments are organized as a binary tree map whose index is the
     /// number of seconds since the start of this ephemeris rounded down
     pub segments: BTreeMap<u32, Segment<S>>,
-    pub start_state: S,
-    pub max_offset: u32,
-    /// Timeout is used to stop listening to new state (default is 10ms).
+    /// Timeout is used to stop listening to new state (default is 50ms, should work well in release and debug mode).
     pub timeout_ms: u64,
+    start_state: S,
+    max_offset: u32,
 }
 
 impl<S: State> Segment<S>
 where
     DefaultAllocator: Allocator<f64, S::PropVecSize> + Allocator<f64, S::Size>,
 {
-    // Evaluate a specific segment
+    /// Evaluate a specific segment at the provided Epoch, requires an initial state as a "template"
     pub fn evaluate(&self, from: S, epoch: Epoch) -> Result<S, NyxError> {
         // Compute the normalized time
         let dur_into_window = epoch - self.start_epoch;
         if dur_into_window > self.duration {
             return Err(NyxError::OutOfInterpolationWindow(format!(
-                "Bug: {} ==> {} - {}",
-                epoch, dur_into_window, self.duration
+                "Requested trajectory at time {} but that is past the final interpolation window by {}",
+                epoch, dur_into_window
             )));
         } else if dur_into_window.in_seconds() < 0.0 {
             // We should not be in this window, but in the next one
             return Err(NyxError::InvalidInterpolationData(format!(
                 "Bug: should be in next window: {}",
-                dur_into_window.in_seconds()
+                dur_into_window
             )));
         }
 
@@ -77,93 +81,39 @@ impl<S: State + 'static> Traj<S>
 where
     DefaultAllocator: Allocator<f64, S::PropVecSize> + Allocator<f64, S::Size>,
 {
+    /// Creates a new trajectory with the provided starting state (used as a template) and a receiving channel.
+    /// The trajectories are always generated on a separate thread.
     pub fn new(state: S, rx: Receiver<S>) -> Result<Self, NyxError> {
         // Initialize the interpolator
         let mut me = Self {
             segments: BTreeMap::new(),
             start_state: state,
-            timeout_ms: 100,
+            timeout_ms: 50,
             max_offset: 0,
         };
 
-        let items_per_segments = 32_usize;
-        let tolerance = 1e-10;
         let mut children = vec![];
-        let mut window_states: Vec<S> = Vec::with_capacity(items_per_segments);
+        let mut window_states: Vec<S> = Vec::with_capacity(ITEMS_PER_SEGMENTS);
         // Push the initial state
         window_states.push(state);
 
         // Start receiving states on a blocking call
         // Note that we're using the typical map+reduce pattern
         while let Ok(state) = rx.recv_timeout(StdDur::from_millis(me.timeout_ms)) {
-            if window_states.len() == items_per_segments {
+            if window_states.len() == ITEMS_PER_SEGMENTS {
                 let this_wdn = window_states.clone();
                 children.push(std::thread::spawn(
-                    move || -> Result<Segment<S>, NyxError> {
-                        // Generate interpolation and flush.
-                        let start_win_epoch = this_wdn[0].epoch();
-                        let end_win_epoch = this_wdn[this_wdn.len() - 1].epoch();
-                        let window_duration = end_win_epoch - start_win_epoch;
-                        let mut ts = Vec::new();
-                        let mut values = Vec::with_capacity(S::PropVecSize::dim());
-                        let mut coefficients = Vec::new();
-                        // Initialize the vector of values and coefficients.
-                        for _ in 0..S::PropVecSize::dim() {
-                            values.push(Vec::with_capacity(items_per_segments));
-                            coefficients.push(Vec::with_capacity(items_per_segments));
-                        }
-                        for state in &this_wdn {
-                            let t_prime = normalize(
-                                (state.epoch() - start_win_epoch).in_seconds(),
-                                -1.0,
-                                window_duration.in_seconds(),
-                            );
-                            ts.push(t_prime);
-                            for (pos, val) in state.as_vector().as_ref().unwrap().iter().enumerate()
-                            {
-                                values[pos].push(*val);
-                            }
-                        }
-
-                        // Generate the polynomials
-                        for (pos, these_values) in values.iter().enumerate() {
-                            match lagrange(&ts, these_values, tolerance) {
-                                Ok(polyn) => coefficients[pos] = polyn.get_coefficients(),
-                                Err(e) => {
-                                    return Err(NyxError::InvalidInterpolationData(format!(
-                                        "Interpolation failed: {}",
-                                        e
-                                    )))
-                                }
-                            };
-                        }
-                        // let poly_x = lagrange(&ts, &xs, tolerance).unwrap();
-                        // // Assert that this polynomial is correct
-                        // // for (n, t) in ts.iter().enumerate() {
-                        // //     let evald = xs[n] - poly_x.evaluate(*t);
-                        // //     let evald_rebuilt = xs[n] - poly_xp.evaluate(*t);
-                        // //     println!("{} km <=> {:.2e} {:.2e}", xs[n], evald, evald_rebuilt,);
-                        // // }
-
-                        // let poly_y = lagrange(&ts, &ys, tolerance).unwrap();
-                        // let poly_z = lagrange(&ts, &zs, tolerance).unwrap();
-                        // let poly_vx = lagrange(&ts, &vxs, tolerance).unwrap();
-                        // let poly_vy = lagrange(&ts, &vys, tolerance).unwrap();
-                        // let poly_vz = lagrange(&ts, &vzs, tolerance).unwrap();
-                        Ok(Segment {
-                            start_epoch: start_win_epoch,
-                            duration: window_duration,
-                            coefficients,
-                            start_state: this_wdn[0],
-                            end_state: this_wdn[items_per_segments - 1],
-                        })
-                    },
+                    move || -> Result<Segment<S>, NyxError> { interpolate(this_wdn) },
                 ));
 
                 window_states.clear();
             }
             window_states.push(state);
         }
+        // And interpolate the remaining states too, even if the buffer is not full!
+        children.push(std::thread::spawn(
+            move || -> Result<Segment<S>, NyxError> { interpolate(window_states) },
+        ));
 
         // Reduce
         for child in children {
@@ -176,7 +126,7 @@ where
     }
 
     fn append_segment(&mut self, segment: Segment<S>) {
-        // Compute the number of seconds since start
+        // Compute the number of seconds since start of trajectory
         let offset_s = ((segment.start_epoch - self.start_state.epoch())
             .in_seconds()
             .floor()) as u32;
@@ -186,6 +136,7 @@ where
         }
     }
 
+    /// Evaluate the trajectory at this specific epoch.
     pub fn evaluate(&self, epoch: Epoch) -> Result<S, NyxError> {
         let offset_s = ((epoch - self.start_state.epoch()).in_seconds().floor()) as u32;
 
@@ -203,14 +154,73 @@ where
             Some((_, segment)) => segment.evaluate(self.start_state, epoch),
         }
     }
+
+    /// Returns the first state in this ephemeris
+    pub fn first(&self) -> S {
+        self.start_state
+    }
+
+    /// Returns the last state in this ephemeris
+    pub fn last(&self) -> S {
+        self.segments[&self.max_offset].end_state
+    }
 }
 
-// Normalize between -1.0 and 1.0
+// Normalize, usually between -1.0 and 1.0
 fn normalize(x: f64, min_x: f64, max_x: f64) -> f64 {
     2.0 * (x - min_x) / (max_x - min_x) - 1.0
 }
 
-// Denormalize between -1.0 and 1.0
+// Denormalize, usually between -1.0 and 1.0
 fn _denormalize(xp: f64, min_x: f64, max_x: f64) -> f64 {
     (max_x - min_x) * (xp + 1.0) / 2.0 + min_x
+}
+
+fn interpolate<S: State + 'static>(this_wdn: Vec<S>) -> Result<Segment<S>, NyxError>
+where
+    DefaultAllocator: Allocator<f64, S::PropVecSize> + Allocator<f64, S::Size>,
+{
+    // Generate interpolation and flush.
+    let start_win_epoch = this_wdn[0].epoch();
+    let end_win_epoch = this_wdn[this_wdn.len() - 1].epoch();
+    let window_duration = end_win_epoch - start_win_epoch;
+    let mut ts = Vec::new();
+    let mut values = Vec::with_capacity(S::PropVecSize::dim());
+    let mut coefficients = Vec::new();
+    // Initialize the vector of values and coefficients.
+    for _ in 0..S::PropVecSize::dim() {
+        values.push(Vec::with_capacity(ITEMS_PER_SEGMENTS));
+        coefficients.push(Vec::with_capacity(ITEMS_PER_SEGMENTS));
+    }
+    for state in &this_wdn {
+        let t_prime = normalize(
+            (state.epoch() - start_win_epoch).in_seconds(),
+            -1.0,
+            window_duration.in_seconds(),
+        );
+        ts.push(t_prime);
+        for (pos, val) in state.as_vector().as_ref().unwrap().iter().enumerate() {
+            values[pos].push(*val);
+        }
+    }
+
+    // Generate the polynomials
+    for (pos, these_values) in values.iter().enumerate() {
+        match lagrange(&ts, these_values, INTERP_TOLERANCE) {
+            Ok(polyn) => coefficients[pos] = polyn.get_coefficients(),
+            Err(e) => {
+                return Err(NyxError::InvalidInterpolationData(format!(
+                    "Interpolation failed: {}",
+                    e
+                )))
+            }
+        };
+    }
+
+    Ok(Segment {
+        start_epoch: start_win_epoch,
+        duration: window_duration,
+        coefficients,
+        end_state: this_wdn[this_wdn.len() - 1],
+    })
 }
--- tests/trajectory.rs
+++ tests/trajectory.rs
@@ -1,76 +1,99 @@
 extern crate nyx_space as nyx;
 
-use nyx::celestia::{Cosm, Orbit};
-use nyx::dynamics::OrbitalDynamics;
+use nyx::celestia::{Cosm, GuidanceMode, Orbit, SpacecraftState};
+use nyx::dynamics::thrustctrl::{Achieve, Ruggiero, ThrustControl, Thruster};
+use nyx::dynamics::{OrbitalDynamics, Spacecraft};
 use nyx::md::{Ephemeris, ScTraj};
 use nyx::propagators::*;
 use nyx::time::{Epoch, TimeSeries, TimeUnit};
-use nyx::State;
+use nyx::{State, TimeTagged};
 use std::sync::mpsc::channel;
 
+#[allow(clippy::identity_op)]
 #[test]
 fn traj_ephem() {
+    // Test that we can correctly interpolate a spacecraft orbit
     let (tx, rx) = channel();
     let cosm = Cosm::de438();
     let eme2k = cosm.frame("EME2000");
 
-    let dt = Epoch::from_gregorian_utc_at_noon(2021, 1, 1);
-    let state = Orbit::cartesian(
-        -2436.45, -2436.45, 6891.037, 5.088_611, -5.088_611, 0.0, dt, eme2k,
+    let start_dt = Epoch::from_gregorian_utc_at_noon(2021, 1, 1);
+    let start_state = Orbit::cartesian(
+        -2436.45, -2436.45, 6891.037, 5.088_611, -5.088_611, 0.0, start_dt, eme2k,
     );
 
-    let ephem_thread = std::thread::spawn(move || Ephemeris::new(state, rx));
-
-    println!("Init: {}", state);
+    // The trajectory must always be generated on its own thread.
+    let ephem_thread = std::thread::spawn(move || Ephemeris::new(start_state, rx));
 
     let dynamics = OrbitalDynamics::two_body();
     let setup = Propagator::rk89(dynamics, PropOpts::<RSSStepPV>::default());
-    let mut prop = setup.with(state).with_tx(tx);
-    prop.for_duration(31 * TimeUnit::Day).unwrap();
+    let mut prop = setup.with(start_state.with_stm()).with_tx(tx);
+    let end_state = prop.for_duration(31 * TimeUnit::Day).unwrap();
 
     // Retrieve the ephemeris by unwrapping it twice:
     // + The first is to unwrap the thread (i.e. assume the thread has not failed)
     // + The second assumes that the generation of the ephemeris didn't fail.
     let ephem = ephem_thread.join().unwrap().unwrap();
 
+    // Example of iterating through the trajectory.
+    // Note how we receive a fully blown Orbit, so we can manipulate it exactly like a normal state.
+    let mut sum_sma = 0.0;
+    let mut cnt = 0.0;
+    for epoch in TimeSeries::inclusive(start_dt, start_dt + 31 * TimeUnit::Day, 1 * TimeUnit::Day) {
+        cnt += 1.0;
+        // Note: the `evaluate` function will return a Result which prevents a panic if you request something out of the ephemeris
+        let state = ephem.evaluate(epoch).unwrap();
+        sum_sma += state.sma();
+    }
+    println!("Average SMA: {:.3} km", sum_sma / cnt);
+
+    // === Below is the validation of the ephemeris == //
+
     assert_eq!(
         ephem.segments.len(),
-        1009,
+        1010,
         "Wrong number of expected segments"
     );
 
-    assert_eq!(ephem.start_state, state, "Wrong initial state");
+    assert_eq!(ephem.first(), start_state, "Wrong initial state");
+    assert_eq!(ephem.last(), end_state, "Wrong final state");
+    assert!(ephem.last().stm().norm() > 0.0, "STM is not set!");
+    assert!(
+        ephem
+            .evaluate(end_state.dt + 1 * TimeUnit::Nanosecond)
+            .is_err(),
+        "Expected to be outside of interpolation window!"
+    );
 
     // Now let's re-generate the truth data and ensure that each state we generate is in the ephemeris and matches the expected state within tolerance.
 
     let (tx, rx) = channel();
     std::thread::spawn(move || {
-        let mut prop = setup.with(state).with_tx(tx);
+        let mut prop = setup.with(start_state).with_tx(tx);
         prop.for_duration(31 * TimeUnit::Day).unwrap();
     });
 
-    let mut max_err = std::f64::NEG_INFINITY;
-    let mut max_pos_err = std::f64::NEG_INFINITY;
-    let mut max_vel_err = std::f64::NEG_INFINITY;
+    // Evaluate the first time of the trajectory to make sure that one is there too.
+    let eval_state = ephem.evaluate(start_dt).unwrap();
+
+    let mut max_pos_err = (eval_state.radius() - start_state.radius()).norm();
+    let mut max_vel_err = (eval_state.velocity() - start_state.velocity()).norm();
+    let mut max_err = (eval_state.as_vector().unwrap() - start_state.as_vector().unwrap()).norm();
 
     while let Ok(prop_state) = rx.recv() {
-        match ephem.evaluate(prop_state.dt) {
-            Ok(eval_state) => {
-                let pos_err = (eval_state.radius() - prop_state.radius()).norm();
-                if pos_err > max_pos_err {
-                    max_pos_err = pos_err;
-                }
-                let vel_err = (eval_state.velocity() - prop_state.velocity()).norm();
-                if vel_err > max_vel_err {
-                    max_vel_err = vel_err;
-                }
-                let err =
-                    (eval_state.as_vector().unwrap() - prop_state.as_vector().unwrap()).norm();
-                if err > max_err {
-                    max_err = err;
-                }
-            }
-            Err(e) => println!("Err: {}", e),
+        let eval_state = ephem.evaluate(prop_state.dt).unwrap();
+
+        let pos_err = (eval_state.radius() - prop_state.radius()).norm();
+        if pos_err > max_pos_err {
+            max_pos_err = pos_err;
+        }
+        let vel_err = (eval_state.velocity() - prop_state.velocity()).norm();
+        if vel_err > max_vel_err {
+            max_vel_err = vel_err;
+        }
+        let err = (eval_state.as_vector().unwrap() - prop_state.as_vector().unwrap()).norm();
+        if err > max_err {
+            max_err = err;
         }
     }
 
@@ -85,12 +108,133 @@ fn traj_ephem() {
         max_err < 1e-10,
         "Maximum orbit in interpolation is too high!"
     );
+}
 
-    // Print a bunch of states throughout the orbit
-    // for epoch in TimeSeries::inclusive(dt, dt + 31 * TimeUnit::Day, 1 * TimeUnit::Day) {
-    //     match ephem.evaluate(epoch) {
-    //         Ok(state) => println!("{}", state),
-    //         Err(e) => println!("{} -- Oh no {}", epoch, e),
-    //     };
-    // }
+#[allow(clippy::identity_op)]
+#[test]
+fn traj_spacecraft() {
+    // Test the interpolation of a spaceraft trajectory and of its fuel. Includes a demo of checking what the guidance mode _should_ be provided the state.
+    // Note that we _do not_ attempt to interpolate the Guidance Mode.
+    // This is based on the Ruggiero AOP correction
+    let (tx, rx) = channel();
+    let cosm = Cosm::de438();
+    let eme2k = cosm.frame("EME2000");
+
+    // Build the initial orbit
+    let sma = eme2k.equatorial_radius() + 900.0;
+    let start_dt = Epoch::from_gregorian_utc_at_noon(2021, 1, 1);
+    let orbit = Orbit::keplerian(sma, 5e-5, 5e-3, 0.0, 178.0, 0.0, start_dt, eme2k);
+
+    // Define the objectives and the control law
+    let objectives = vec![Achieve::Aop {
+        target: 183.0,
+        tol: 5e-3,
+    }];
+
+    let ruggiero_ctrl = Ruggiero::new(objectives, orbit);
+
+    // Build the spacecraft state
+    let fuel_mass = 67.0;
+    let dry_mass = 300.0;
+    // Define the thruster
+    let lowt = Thruster {
+        thrust: 89e-3,
+        isp: 1650.0,
+    };
+    let start_state =
+        SpacecraftState::with_thruster(orbit, dry_mass, fuel_mass, lowt, GuidanceMode::Thrust);
+
+    let sc_dynamics = Spacecraft::with_ctrl(OrbitalDynamics::two_body(), ruggiero_ctrl.clone());
+
+    // The trajectory must always be generated on its own thread.
+    let ephem_thread = std::thread::spawn(move || ScTraj::new(start_state, rx));
+
+    let setup = Propagator::default(sc_dynamics);
+    let prop_time = 44 * TimeUnit::Minute + 10 * TimeUnit::Second;
+    let mut prop = setup.with(start_state).with_tx(tx);
+    let end_state = prop.for_duration(prop_time).unwrap();
+
+    // Retrieve the ephemeris by unwrapping it twice:
+    // + The first is to unwrap the thread (i.e. assume the thread has not failed)
+    // + The second assumes that the generation of the ephemeris didn't fail.
+    let ephem = ephem_thread.join().unwrap().unwrap();
+
+    // Example of iterating through the spaceraft trajectory and checking what the guidance mode is at each time.
+    let mut prev_mode = GuidanceMode::Coast;
+    for epoch in TimeSeries::inclusive(start_dt, start_dt + prop_time, 1 * TimeUnit::Day) {
+        // Note: the `evaluate` function will return a Result which prevents a panic if you request something out of the ephemeris
+        let sc_state = ephem.evaluate(epoch).unwrap();
+        let mode_then = ruggiero_ctrl.next(&sc_state);
+        if mode_then != prev_mode {
+            println!(
+                "Mode changed from {:?} to {:?} @ {}",
+                prev_mode, mode_then, epoch
+            );
+            prev_mode = mode_then;
+        }
+    }
+
+    // === Below is the validation of the ephemeris == //
+
+    assert_eq!(ephem.segments.len(), 2, "Wrong number of expected segments");
+
+    assert_eq!(ephem.first(), start_state, "Wrong initial state");
+    assert_eq!(ephem.last(), end_state, "Wrong final state");
+    assert!(
+        ephem
+            .evaluate(end_state.epoch() + 1 * TimeUnit::Nanosecond)
+            .is_err(),
+        "Expected to be outside of interpolation window!"
+    );
+
+    // Now let's re-generate the truth data and ensure that each state we generate is in the ephemeris and matches the expected state within tolerance.
+
+    let (tx, rx) = channel();
+    std::thread::spawn(move || {
+        let mut prop = setup.with(start_state).with_tx(tx);
+        prop.for_duration(prop_time).unwrap();
+    });
+
+    // Evaluate the first time of the trajectory to make sure that one is there too.
+    let eval_state = ephem.evaluate(start_dt).unwrap();
+
+    let mut max_pos_err = (eval_state.orbit.radius() - start_state.orbit.radius()).norm();
+    let mut max_vel_err = (eval_state.orbit.velocity() - start_state.orbit.velocity()).norm();
+    let mut max_fuel_err = eval_state.fuel_mass_kg - start_state.fuel_mass_kg;
+    let mut max_err = (eval_state.as_vector().unwrap() - start_state.as_vector().unwrap()).norm();
+
+    while let Ok(prop_state) = rx.recv() {
+        let eval_state = ephem.evaluate(prop_state.epoch()).unwrap();
+        println!("{}", eval_state);
+
+        let pos_err = (eval_state.orbit.radius() - prop_state.orbit.radius()).norm();
+        if pos_err > max_pos_err {
+            max_pos_err = pos_err;
+        }
+        let vel_err = (eval_state.orbit.velocity() - prop_state.orbit.velocity()).norm();
+        if vel_err > max_vel_err {
+            max_vel_err = vel_err;
+        }
+        let fuel_err = eval_state.fuel_mass_kg - prop_state.fuel_mass_kg;
+        if fuel_err > max_fuel_err {
+            max_fuel_err = fuel_err;
+        }
+        let err = (eval_state.as_vector().unwrap() - prop_state.as_vector().unwrap()).norm();
+        if err > max_err {
+            max_err = err;
+        }
+    }
+
+    println!(
+        "[traj_ephem] Maximum interpolation error: pos: {:.2e} m\t\tvel: {:.2e} m/s\t\tfuel: {:.2e} g\t\tfull state: {:.2e} (no unit)",
+        max_pos_err * 1e3,
+        max_vel_err * 1e3,
+        max_fuel_err * 1e3,
+        max_err
+    );
+
+    assert!(
+        max_err < 1e-10,
+        "Maximum orbit in interpolation is too high!"
+    );
 }
