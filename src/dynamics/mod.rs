265d52f924393c98722f1acec958a6f3fe72faf6
Christopher Rabotin <christopher.rabotin@gmail.com>
2021-02-11 01:35:15 -0700

    Change license to AGPLv3.0
    Signed-off-by: Christopher Rabotin <christopher.rabotin@gmail.com>

--- src/dynamics/mod.rs
+++ src/dynamics/mod.rs
@@ -1,3 +1,21 @@
+/*
+    Nyx, blazing fast astrodynamics
+    Copyright (C) 2021 Christopher Rabotin <christopher.rabotin@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 extern crate hyperdual;
 
 use self::hyperdual::{hyperspace_from_vector, Hyperdual, Owned};
