f827e865e720513cc0627dabde614ea840a9ec02
Christopher Rabotin <christopher.rabotin@gmail.com>
2021-03-25 09:48:37 -0600

    dv errors function generalized to any current pointing vector
    Signed-off-by: Christopher Rabotin <christopher.rabotin@gmail.com>

--- src/mc/helpers.rs
+++ src/mc/helpers.rs
@@ -1,6 +1,6 @@
 use super::{Distribution, Normal, Rng, Uniform};
 use crate::dimensions::Vector3;
-use crate::{NyxError, Orbit};
+use crate::NyxError;
 
 /// Returns a unit vector from a normal distribution.
 /// Implements the Sphere Point Picking method: https://mathworld.wolfram.com/SpherePointPicking.html
@@ -17,7 +17,7 @@ pub fn unit_vector_from_seed<R: Rng>(rng: &mut R) -> Vector3<f64> {
 /// Requires a 3-sigma error percentage (e.g. 0.05 is a 5% pointing error) and a pseudo random number generator
 /// Returns the delta-v vector with the applying pointing error
 pub fn dv_pointing_error<R: Rng>(
-    orbit: &Orbit,
+    cur_pointing: &Vector3<f64>,
     dv: Vector3<f64>,
     error_prct3s: f64,
     rng: &mut R,
@@ -38,7 +38,8 @@ pub fn dv_pointing_error<R: Rng>(
     }
 
     let dv_hat = dv / dv_mag;
-    let cur_angle = (orbit.velocity().dot(&dv_hat) / orbit.vmag()).acos();
+    let cur_pointing_mag = cur_pointing.norm();
+    let cur_angle = (cur_pointing.dot(&dv_hat) / cur_pointing_mag).acos();
 
     let new_angle = Normal::new(cur_angle, error_prct3s / 3.0)
         .unwrap()
@@ -49,13 +50,13 @@ pub fn dv_pointing_error<R: Rng>(
 
 /// Returns the provided Delta V vector with a magnitude and pointing error
 pub fn dv_execution_error<R: Rng>(
-    orbit: &Orbit,
+    cur_pointing: &Vector3<f64>,
     dv: Vector3<f64>,
     pointing_3s: f64,
     mag_3s: f64,
     rng: &mut R,
 ) -> Result<Vector3<f64>, NyxError> {
-    let dv = dv_pointing_error(orbit, dv, pointing_3s, rng)?;
+    let dv = dv_pointing_error(cur_pointing, dv, pointing_3s, rng)?;
 
     let new_mag = Normal::new(dv.norm(), mag_3s / 3.0).unwrap().sample(rng);
 
@@ -65,7 +66,7 @@ pub fn dv_execution_error<R: Rng>(
 #[test]
 fn test_dv_mag_fixed() {
     use super::thread_rng;
-    use crate::celestia::Cosm;
+    use crate::celestia::{Cosm, Orbit};
     use crate::time::Epoch;
     let cosm = Cosm::de438();
     let eme2k = cosm.frame("EME2000");
@@ -87,7 +88,7 @@ fn test_dv_mag_fixed() {
         let dv_mag = dv_mag_distr.sample(&mut thread_rng());
         let dv_point = unit_vector_from_seed(&mut thread_rng());
         let dv = dv_point * dv_mag;
-        let dv_w_err = dv_pointing_error(&orbit, dv, 0.1, &mut thread_rng()).unwrap();
+        let dv_w_err = dv_pointing_error(&orbit.velocity(), dv, 0.1, &mut thread_rng()).unwrap();
         assert!(
             (dv_w_err.norm() - dv_mag) < std::f64::EPSILON,
             "{:.1e}",
