8f5175b31fefda73afe403931059f86236ab191b
Christopher Rabotin <christopher.rabotin@gmail.com>
2021-05-31 16:00:38 -0600

    Add ecc tgt
    Signed-off-by: Christopher Rabotin <christopher.rabotin@gmail.com>

--- tests/mission_design/targeter/single_oe.rs
+++ tests/mission_design/targeter/single_oe.rs
@@ -3,6 +3,8 @@ extern crate nyx_space as nyx;
 use nyx::md::targeter::*;
 use nyx::md::ui::*;
 
+// Semi major axis
+
 #[test]
 fn tgt_sma_from_apo() {
     if pretty_env_logger::try_init().is_err() {
@@ -132,5 +134,119 @@ fn tgt_sma_from_peri() {
         "GMAT validation - tgt_sma_from_peri: Δv = {:.3} m/s\terr = {:.6} m/s",
         solution_fd.correction.norm() * 1e3,
         (solution_fd.correction.norm() - gmat_sol).abs() * 1e3
+    );
+}
+
+// Eccentricity
+#[test]
+fn tgt_ecc_from_apo() {
+    if pretty_env_logger::try_init().is_err() {
+        println!("could not init env_logger");
+    }
+
+    let cosm = Cosm::de438();
+    let eme2k = cosm.frame("EME2000");
+
+    let orig_dt = Epoch::from_gregorian_utc_at_midnight(2020, 1, 1);
+
+    let xi_orig = Orbit::keplerian(8_000.0, 0.2, 30.0, 60.0, 60.0, 180.0, orig_dt, eme2k);
+
+    let target_delta_t: Duration = xi_orig.period() / 2.0;
+
+    println!("Period: {} s", xi_orig.period().in_seconds() / 2.0);
+
+    let spacecraft = Spacecraft::from_srp_defaults(xi_orig, 100.0, 0.0);
+
+    let dynamics = SpacecraftDynamics::new(OrbitalDynamics::two_body());
+    let setup = Propagator::default(dynamics);
+
+    let xf_desired_ecc = 0.4;
+
+    // Define the objective
+    let objectives = vec![Objective::new(StateParameter::Eccentricity, xf_desired_ecc)];
+
+    let tgt = Targeter::delta_v(Arc::new(&setup), objectives);
+
+    println!("{}", tgt);
+
+    let solution_fd = tgt
+        .try_achieve_from_with_guess(
+            spacecraft,
+            &[0.0, 0.0, 0.0],
+            orig_dt,
+            orig_dt + target_delta_t,
+        )
+        .unwrap();
+
+    println!("Finite differencing solution: {}", solution_fd);
+
+    let gmat_sol = 0.7721483022815125;
+    println!(
+        "GMAT validation - tgt_ecc_from_apo: Δv = {:.3} m/s\terr = {:.6} m/s",
+        solution_fd.correction.norm() * 1e3,
+        (solution_fd.correction.norm() - gmat_sol).abs() * 1e3
+    );
+    // GMAT validation
+    assert!(
+        (solution_fd.correction.norm() - gmat_sol).abs() < 1e-3,
+        "Finite differencing result different from GMAT (greater than 1 m/s)."
+    );
+}
+
+#[test]
+fn tgt_ecc_from_peri() {
+    if pretty_env_logger::try_init().is_err() {
+        println!("could not init env_logger");
+    }
+
+    let cosm = Cosm::de438();
+    let eme2k = cosm.frame("EME2000");
+
+    let orig_dt = Epoch::from_gregorian_utc_at_midnight(2020, 1, 1);
+
+    let xi_orig = Orbit::keplerian(8_000.0, 0.2, 30.0, 60.0, 60.0, 0.0, orig_dt, eme2k);
+
+    let target_delta_t: Duration = xi_orig.period() / 2.0;
+
+    println!("Period: {} s", xi_orig.period().in_seconds() / 2.0);
+
+    let spacecraft = Spacecraft::from_srp_defaults(xi_orig, 100.0, 0.0);
+
+    let dynamics = SpacecraftDynamics::new(OrbitalDynamics::point_masses(
+        &[Bodies::Luna, Bodies::Sun, Bodies::JupiterBarycenter],
+        cosm,
+    ));
+    let setup = Propagator::default(dynamics);
+
+    let xf_desired_ecc = 0.4;
+
+    // Define the objective
+    let objectives = vec![Objective::new(StateParameter::Eccentricity, xf_desired_ecc)];
+
+    let tgt = Targeter::delta_v(Arc::new(&setup), objectives);
+
+    println!("{}", tgt);
+
+    let solution_fd = tgt
+        .try_achieve_from_with_guess(
+            spacecraft,
+            &[0.0, 0.0, 0.0],
+            orig_dt,
+            orig_dt + target_delta_t,
+        )
+        .unwrap();
+
+    println!("Finite differencing solution: {}", solution_fd);
+
+    let gmat_sol = 0.6926746704643234;
+    println!(
+        "GMAT validation - tgt_sma_from_peri: Δv = {:.3} m/s\terr = {:.6} m/s",
+        solution_fd.correction.norm() * 1e3,
+        (solution_fd.correction.norm() - gmat_sol).abs() * 1e3
+    );
+    // GMAT validation
+    assert!(
+        (solution_fd.correction.norm() - gmat_sol).abs() < 1e-3,
+        "Finite differencing result different from GMAT (greater than 1 m/s)."
     );
 }
