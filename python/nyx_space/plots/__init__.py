486bc68cd03a6d30113080aa7f8f4f8eae68d19b
Christopher Rabotin <christopher.rabotin@gmail.com>
2023-03-30 18:36:15 -0600

    Add plotting functions to the module

--- python/nyx_space/plots/__init__.py
+++ python/nyx_space/plots/__init__.py
@@ -1,3 +1,4 @@
 from .gauss_markov import plot_gauss_markov
+from .od import plot_covar, plot_estimates, plot_state_deviation
 
-__all__ = ["plot_gauss_markov"]
+__all__ = ["plot_gauss_markov", "plot_covar", "plot_estimates", "plot_state_deviation"]
