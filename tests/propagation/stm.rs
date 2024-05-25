43b9414fd12398d02f12dfea4429b5afa860fffb
Christopher Rabotin <christopher.rabotin@gmail.com>
2024-05-24 19:08:49 -0600

    Fixed st

--- tests/propagation/stm.rs
+++ tests/propagation/stm.rs
@@ -19,13 +19,13 @@ use rstest::*;
 use crate::propagation::GMAT_EARTH_GM;
 
 #[fixture]
-fn almanac() -> Almanac {
-    use crate::test_almanac;
-    test_almanac()
+fn almanac() -> Arc<Almanac> {
+    use crate::test_almanac_arcd;
+    test_almanac_arcd()
 }
 
 #[rstest]
-fn stm_fixed_step(almanac: Almanac) {
+fn stm_fixed_step(almanac: Arc<Almanac>) {
     let eme2k = almanac
         .frame_from_uid(EARTH_J2000)
         .unwrap()
@@ -42,16 +42,20 @@ fn stm_fixed_step(almanac: Almanac) {
     let eccs = vec![1e-5, 0.2];
 
     for ecc in eccs {
-        let init = Orbit::keplerian(8000.0, ecc, 10.0, 5.0, 25.0, 0.0, epoch, eme2k);
+        let init = Spacecraft::from(Orbit::keplerian(
+            8000.0, ecc, 10.0, 5.0, 25.0, 0.0, epoch, eme2k,
+        ));
 
         let ten_steps = prop
-            .with(init.with_stm())
+            .with(init.with_stm(), almanac.clone())
             .for_duration(100 * Unit::Second)
             .unwrap();
 
-        let nominal = ten_steps.to_cartesian_pos_vel();
+        let nominal = ten_steps.orbit.to_cartesian_pos_vel();
 
-        let stm_err_ten_steps = ten_steps.stm().unwrap() * init.to_cartesian_pos_vel() - nominal;
+        let stm_err_ten_steps = ten_steps.stm().unwrap().fixed_resize::<6, 6>(0.0)
+            * init.orbit.to_cartesian_pos_vel()
+            - nominal;
 
         println!("HD ten_steps.stm() = {}", ten_steps.stm().unwrap());
 
@@ -64,21 +68,21 @@ fn stm_fixed_step(almanac: Almanac) {
         for i in 0..6 {
             let mut this_init = init.with_stm();
             match i {
-                0 => this_init.x_km += pert,
-                1 => this_init.y_km += pert,
-                2 => this_init.z_km += pert,
-                3 => this_init.vx_km_s += pert,
-                4 => this_init.vy_km_s += pert,
-                5 => this_init.vz_km_s += pert,
+                0 => this_init.orbit.radius_km.x += pert,
+                1 => this_init.orbit.radius_km.y += pert,
+                2 => this_init.orbit.radius_km.z += pert,
+                3 => this_init.orbit.velocity_km_s.x += pert,
+                4 => this_init.orbit.velocity_km_s.y += pert,
+                5 => this_init.orbit.velocity_km_s.z += pert,
                 _ => unreachable!(),
             }
 
             let these_ten_steps = prop
-                .with(this_init)
+                .with(this_init, almanac.clone())
                 .for_duration(100 * Unit::Second)
                 .unwrap();
 
-            let jac_val = (these_ten_steps.to_cartesian_pos_vel() - nominal) / pert;
+            let jac_val = (these_ten_steps.orbit.to_cartesian_pos_vel() - nominal) / pert;
 
             for j in 0..6 {
                 stm_fd[(j, i)] = jac_val[j];
@@ -87,7 +91,7 @@ fn stm_fixed_step(almanac: Almanac) {
 
         println!("FD stm_fd = {}", stm_fd);
 
-        let stm_fd_err_ten_steps = stm_fd * init.to_cartesian_pos_vel() - nominal;
+        let stm_fd_err_ten_steps = stm_fd * init.orbit.to_cartesian_pos_vel() - nominal;
 
         println!("FD stm_fd_err_ten_steps = {}", stm_fd_err_ten_steps);
 
@@ -105,7 +109,7 @@ fn stm_fixed_step(almanac: Almanac) {
 }
 
 #[rstest]
-fn stm_variable_step(almanac: Almanac) {
+fn stm_variable_step(almanac: Arc<Almanac>) {
     let eme2k = almanac
         .frame_from_uid(EARTH_J2000)
         .unwrap()
@@ -119,16 +123,20 @@ fn stm_variable_step(almanac: Almanac) {
     // First test is in mostly linear regime (low eccentricity)
     // The second is quite non linear because the eccentricity is 0.2 and we start at periapsis
     for ecc in eccs {
-        let init = Orbit::keplerian(8000.0, ecc, 10.0, 5.0, 25.0, 0.0, epoch, eme2k);
+        let init = Spacecraft::from(Orbit::keplerian(
+            8000.0, ecc, 10.0, 5.0, 25.0, 0.0, epoch, eme2k,
+        ));
 
         let ten_steps = prop
-            .with(init.with_stm())
+            .with(init.with_stm(), almanac.clone())
             .for_duration(100 * Unit::Second)
             .unwrap();
 
-        let nominal = ten_steps.to_cartesian_pos_vel();
+        let nominal = ten_steps.orbit.to_cartesian_pos_vel();
 
-        let stm_err_ten_steps = ten_steps.stm().unwrap() * init.to_cartesian_pos_vel() - nominal;
+        let stm_err_ten_steps = ten_steps.stm().unwrap().fixed_resize::<6, 6>(0.0)
+            * init.orbit.to_cartesian_pos_vel()
+            - nominal;
 
         println!("HD ten_steps.stm() = {}", ten_steps.stm().unwrap());
 
@@ -141,21 +149,21 @@ fn stm_variable_step(almanac: Almanac) {
         for i in 0..6 {
             let mut this_init = init.with_stm();
             match i {
-                0 => this_init.x_km += pert,
-                1 => this_init.y_km += pert,
-                2 => this_init.z_km += pert,
-                3 => this_init.vx_km_s += pert,
-                4 => this_init.vy_km_s += pert,
-                5 => this_init.vz_km_s += pert,
+                0 => this_init.orbit.radius_km.x += pert,
+                1 => this_init.orbit.radius_km.y += pert,
+                2 => this_init.orbit.radius_km.z += pert,
+                3 => this_init.orbit.velocity_km_s.x += pert,
+                4 => this_init.orbit.velocity_km_s.y += pert,
+                5 => this_init.orbit.velocity_km_s.z += pert,
                 _ => unreachable!(),
             }
 
             let these_ten_steps = prop
-                .with(this_init)
+                .with(this_init, almanac.clone())
                 .for_duration(100 * Unit::Second)
                 .unwrap();
 
-            let jac_val = (these_ten_steps.to_cartesian_pos_vel() - nominal) / pert;
+            let jac_val = (these_ten_steps.orbit.to_cartesian_pos_vel() - nominal) / pert;
 
             for j in 0..6 {
                 stm_fd[(j, i)] = jac_val[j];
@@ -164,7 +172,7 @@ fn stm_variable_step(almanac: Almanac) {
 
         println!("FD stm_fd = {}", stm_fd);
 
-        let stm_fd_err_ten_steps = stm_fd * init.to_cartesian_pos_vel() - nominal;
+        let stm_fd_err_ten_steps = stm_fd * init.orbit.to_cartesian_pos_vel() - nominal;
 
         println!("FD stm_fd_err_ten_steps = {}", stm_fd_err_ten_steps);
 
@@ -182,7 +190,7 @@ fn stm_variable_step(almanac: Almanac) {
 }
 
 #[rstest]
-fn stm_between_steps(almanac: Almanac) {
+fn stm_between_steps(almanac: Arc<Almanac>) {
     // Check that \Phi(t_2, t_1) = \Phi(t_2, t_0) * \Phi^{-1}(t_1, t_0)
 
     let eme2k = almanac
@@ -198,17 +206,29 @@ fn stm_between_steps(almanac: Almanac) {
     // First test is in mostly linear regime (low eccentricity)
     // The second is quite non linear because the eccentricity is 0.2 and we start at periapsis
     for ecc in eccs {
-        let init = Orbit::keplerian(8000.0, ecc, 10.0, 5.0, 25.0, 0.0, epoch, eme2k).with_stm();
+        let init = Spacecraft::from(Orbit::keplerian(
+            8000.0, ecc, 10.0, 5.0, 25.0, 0.0, epoch, eme2k,
+        ))
+        .with_stm();
 
-        let t100 = prop.with(init).for_duration(100 * Unit::Second).unwrap();
+        let t100 = prop
+            .with(init, almanac.clone())
+            .for_duration(100 * Unit::Second)
+            .unwrap();
 
         let phi_t100_t0 = t100.stm().unwrap();
 
-        let t50 = prop.with(init).for_duration(50 * Unit::Second).unwrap();
+        let t50 = prop
+            .with(init, almanac.clone())
+            .for_duration(50 * Unit::Second)
+            .unwrap();
 
         let phi_t50_t0 = t50.stm().unwrap();
 
-        let t50_to_t100 = prop.with(t50).for_duration(50 * Unit::Second).unwrap();
+        let t50_to_t100 = prop
+            .with(t50, almanac.clone())
+            .for_duration(50 * Unit::Second)
+            .unwrap();
 
         let phi_t100_t50 = t50_to_t100.stm().unwrap();
 
@@ -228,7 +248,7 @@ fn stm_between_steps(almanac: Almanac) {
 }
 
 #[rstest]
-fn stm_hifi_variable_step(almanac: Almanac) {
+fn stm_hifi_variable_step(almanac: Arc<Almanac>) {
     // Using higher fidelity dynamics for STM testing
 
     let eme2k = almanac
@@ -237,23 +257,29 @@ fn stm_hifi_variable_step(almanac: Almanac) {
         .with_mu_km3_s2(GMAT_EARTH_GM);
     let epoch = Epoch::from_gregorian_tai_at_midnight(2020, 1, 1);
 
-    let prop = Propagator::default_dp78(OrbitalDynamics::point_masses(vec![MOON, SUN]));
+    let prop = Propagator::default_dp78(SpacecraftDynamics::new(OrbitalDynamics::point_masses(
+        vec![MOON, SUN],
+    )));
 
     let eccs = vec![1e-5, 0.2];
 
     // First test is in mostly linear regime (low eccentricity)
     // The second is quite non linear because the eccentricity is 0.2 and we start at periapsis
     for ecc in eccs {
-        let init = Orbit::keplerian(8000.0, ecc, 10.0, 5.0, 25.0, 0.0, epoch, eme2k);
+        let init = Spacecraft::from(Orbit::keplerian(
+            8000.0, ecc, 10.0, 5.0, 25.0, 0.0, epoch, eme2k,
+        ));
 
         let ten_steps = prop
-            .with(init.with_stm())
+            .with(init.with_stm(), almanac.clone())
             .for_duration(100 * Unit::Second)
             .unwrap();
 
-        let nominal = ten_steps.to_cartesian_pos_vel();
+        let nominal = ten_steps.orbit.to_cartesian_pos_vel();
 
-        let stm_err_ten_steps = ten_steps.stm().unwrap() * init.to_cartesian_pos_vel() - nominal;
+        let stm_err_ten_steps = ten_steps.stm().unwrap().fixed_resize::<6, 6>(0.0)
+            * init.orbit.to_cartesian_pos_vel()
+            - nominal;
 
         println!("HD ten_steps.stm() = {}", ten_steps.stm().unwrap());
 
@@ -266,21 +292,21 @@ fn stm_hifi_variable_step(almanac: Almanac) {
         for i in 0..6 {
             let mut this_init = init.with_stm();
             match i {
-                0 => this_init.x_km += pert,
-                1 => this_init.y_km += pert,
-                2 => this_init.z_km += pert,
-                3 => this_init.vx_km_s += pert,
-                4 => this_init.vy_km_s += pert,
-                5 => this_init.vz_km_s += pert,
+                0 => this_init.orbit.radius_km.x += pert,
+                1 => this_init.orbit.radius_km.y += pert,
+                2 => this_init.orbit.radius_km.z += pert,
+                3 => this_init.orbit.velocity_km_s.x += pert,
+                4 => this_init.orbit.velocity_km_s.y += pert,
+                5 => this_init.orbit.velocity_km_s.z += pert,
                 _ => unreachable!(),
             }
 
             let these_ten_steps = prop
-                .with(this_init)
+                .with(this_init, almanac.clone())
                 .for_duration(100 * Unit::Second)
                 .unwrap();
 
-            let jac_val = (these_ten_steps.to_cartesian_pos_vel() - nominal) / pert;
+            let jac_val = (these_ten_steps.orbit.to_cartesian_pos_vel() - nominal) / pert;
 
             for j in 0..6 {
                 stm_fd[(j, i)] = jac_val[j];
@@ -289,7 +315,7 @@ fn stm_hifi_variable_step(almanac: Almanac) {
 
         println!("FD stm_fd = {}", stm_fd);
 
-        let stm_fd_err_ten_steps = stm_fd * init.to_cartesian_pos_vel() - nominal;
+        let stm_fd_err_ten_steps = stm_fd * init.orbit.to_cartesian_pos_vel() - nominal;
 
         println!("FD stm_fd_err_ten_steps = {}", stm_fd_err_ten_steps);
 
@@ -307,20 +333,22 @@ fn stm_hifi_variable_step(almanac: Almanac) {
 }
 
 #[rstest]
-fn orbit_set_unset_static(almanac: Almanac) {
+fn orbit_set_unset_static(almanac: Arc<Almanac>) {
     let eme2k = almanac
         .frame_from_uid(EARTH_J2000)
         .unwrap()
         .with_mu_km3_s2(GMAT_EARTH_GM);
     let epoch = Epoch::from_gregorian_tai_at_midnight(2020, 1, 1);
 
-    let mut init = Orbit::keplerian(8000.0, 0.5, 10.0, 5.0, 25.0, 0.0, epoch, eme2k).with_stm();
+    let mut init = Spacecraft::from(
+        Orbit::keplerian(8000.0, 0.5, 10.0, 5.0, 25.0, 0.0, epoch, eme2k),
+    ).with_stm();
 
     // Change STM
     let stm_data = (0..36).map(|x| x as f64).collect::<Vec<f64>>();
     init.stm.as_mut().unwrap().copy_from_slice(&stm_data);
 
-    let init_vec = init.as_vector();
+    let init_vec = init.to_vector();
 
     let mut init2 = init;
     init2.set(epoch, &init_vec);
@@ -329,20 +357,28 @@ fn orbit_set_unset_static(almanac: Almanac) {
 }
 
 #[rstest]
-fn orbit_set_unset(almanac: Almanac) {
+fn orbit_set_unset(almanac: Arc<Almanac>) {
     let eme2k = almanac
         .frame_from_uid(EARTH_J2000)
         .unwrap()
         .with_mu_km3_s2(GMAT_EARTH_GM);
     let epoch = Epoch::from_gregorian_tai_at_midnight(2020, 1, 1);
 
-    let init = Orbit::keplerian(8000.0, 0.5, 10.0, 5.0, 25.0, 0.0, epoch, eme2k).with_stm();
+    let init = Spacecraft::from(Orbit::keplerian(
+        8000.0, 0.5, 10.0, 5.0, 25.0, 0.0, epoch, eme2k,
+    ))
+    .with_stm();
 
-    let prop = Propagator::default_dp78(OrbitalDynamics::point_masses(vec![MOON, SUN]));
+    let prop = Propagator::default_dp78(SpacecraftDynamics::new(OrbitalDynamics::point_masses(
+        vec![MOON, SUN],
+    )));
 
-    let orbit = prop.with(init).for_duration(2 * Unit::Hour).unwrap();
+    let orbit = prop
+        .with(init, almanac.clone())
+        .for_duration(2 * Unit::Hour)
+        .unwrap();
 
-    let vec = orbit.as_vector();
+    let vec = orbit.to_vector();
 
     let mut init2 = orbit;
     init2.set(orbit.epoch(), &vec);
@@ -351,20 +387,20 @@ fn orbit_set_unset(almanac: Almanac) {
 }
 
 #[rstest]
-fn sc_set_unset_static(almanac: Almanac) {
+fn sc_set_unset_static(almanac: Arc<Almanac>) {
     let eme2k = almanac
         .frame_from_uid(EARTH_J2000)
         .unwrap()
         .with_mu_km3_s2(GMAT_EARTH_GM);
     let epoch = Epoch::from_gregorian_tai_at_midnight(2020, 1, 1);
 
-    let init = Orbit::keplerian(8000.0, 0.5, 10.0, 5.0, 25.0, 0.0, epoch, eme2k).with_stm();
-    let mut init_sc = Spacecraft::from_srp_defaults(init, 100.0, 1.0);
+    let init = Orbit::keplerian(8000.0, 0.5, 10.0, 5.0, 25.0, 0.0, epoch, eme2k);
+    let mut init_sc = Spacecraft::from_srp_defaults(init, 100.0, 1.0).with_stm();
 
     // Change the full vector
     let data = (0..90).map(|x| x as f64).collect::<Vec<f64>>();
     init_sc.set(
-        init.epoch(),
+        init.epoch,
         &OVector::<f64, Const<90>>::from_column_slice(&data),
     );
 
@@ -374,44 +410,4 @@ fn sc_set_unset_static(almanac: Almanac) {
     init2.set(epoch, &init_vec);
 
     assert_eq!(init_sc, init2);
-}
-
-#[rstest]
-fn sc_and_orbit_stm_chk(almanac: Almanac) {
-    let eme2k = almanac
-        .frame_from_uid(EARTH_J2000)
-        .unwrap()
-        .with_mu_km3_s2(GMAT_EARTH_GM);
-    let epoch = Epoch::from_gregorian_tai_at_midnight(2020, 1, 1);
-
-    let init_orbit = Orbit::keplerian(8000.0, 0.5, 10.0, 5.0, 25.0, 0.0, epoch, eme2k).with_stm();
-    let init_sc = Spacecraft::from_srp_defaults(init_orbit, 0.0, 0.0);
-
-    let prop_orbit = Propagator::default_dp78(OrbitalDynamics::point_masses(vec![MOON, SUN]));
-
-    let prop_sc = Propagator::default_dp78(SpacecraftDynamics::new(OrbitalDynamics::point_masses(
-        vec![MOON, SUN],
-    )));
-
-    let final_orbit = prop_orbit
-        .with(init_orbit)
-        .for_duration(2 * Unit::Hour)
-        .unwrap();
-    let final_sc = prop_sc
-        .with(init_sc, Arc::new(almanac))
-        .for_duration(2 * Unit::Hour)
-        .unwrap();
-
-    assert_eq!(
-        final_orbit, final_sc.orbit,
-        "Identical dynamics for Spacecraft and Orbit lead to different states"
-    );
-
-    let sc_stm = final_sc.stm().unwrap();
-    let sc_orbit_stm = sc_stm.fixed_view::<6, 6>(0, 0);
-    assert_eq!(
-        sc_orbit_stm,
-        final_orbit.stm().unwrap(),
-        "Identical dynamics for Spacecraft and Orbit lead to different STM"
-    );
 }
