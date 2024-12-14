6983a675feb5d140949b5dab52c94b589ce424b7
Christopher Rabotin <christopher.rabotin@gmail.com>
2024-12-14 12:05:34 -0700

    Add TDM exporting

--- src/od/msr/trackingdata/io_ccsds_tdm.rs
+++ src/od/msr/trackingdata/io_ccsds_tdm.rs
@@ -16,17 +16,20 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+use crate::io::watermark::prj_name_ver;
 use crate::io::ExportCfg;
 use crate::io::{InputOutputError, StdIOSnafu};
 use crate::od::msr::{Measurement, MeasurementType};
+use hifitime::efmt::{Format, Formatter};
 use hifitime::prelude::Epoch;
 use hifitime::TimeScale;
 use snafu::ResultExt;
 use std::collections::{BTreeMap, HashMap};
-use std::error::Error;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::Write;
+use std::io::{BufRead, BufReader, BufWriter};
 use std::path::{Path, PathBuf};
+use std::str::FromStr;
 
 use super::TrackingDataArc;
 
@@ -107,12 +110,169 @@ impl TrackingDataArc {
     }
 
     /// Store this tracking arc to a CCSDS TDM file, with optional metadata and a timestamp appended to the filename.
-    pub fn to_tdm<P: AsRef<Path>>(
-        &self,
+    pub fn to_tdm_file<P: AsRef<Path>>(
+        mut self,
+        spacecraft_name: String,
+        aliases: Option<HashMap<String, String>>,
         path: P,
         cfg: ExportCfg,
-    ) -> Result<PathBuf, Box<dyn Error>> {
-        todo!()
+    ) -> Result<PathBuf, InputOutputError> {
+        if self.is_empty() {
+            return Err(InputOutputError::MissingData {
+                which: " - empty tracking data cannot be exported to TDM".to_string(),
+            });
+        }
+
+        // Filter epochs if needed.
+        if cfg.start_epoch.is_some() && cfg.end_epoch.is_some() {
+            self = self.filter_by_epoch(cfg.start_epoch.unwrap()..cfg.end_epoch.unwrap());
+        } else if cfg.start_epoch.is_some() {
+            self = self.filter_by_epoch(cfg.start_epoch.unwrap()..);
+        } else if cfg.end_epoch.is_some() {
+            self = self.filter_by_epoch(..cfg.end_epoch.unwrap());
+        }
+
+        let tick = Epoch::now().unwrap();
+        info!("Exporting tracking data to CCSDS TDM file...");
+
+        // Grab the path here before we move stuff.
+        let path_buf = cfg.actual_path(path);
+
+        let metadata = cfg.metadata.unwrap_or_default();
+
+        let file = File::create(&path_buf).context(StdIOSnafu {
+            action: "creating CCSDS TDM file for tracking arc",
+        })?;
+        let mut writer = BufWriter::new(file);
+
+        let err_hdlr = |source| InputOutputError::StdIOError {
+            source,
+            action: "writing data to TDM file",
+        };
+
+        // Epoch formmatter.
+        let iso8601_no_ts = Format::from_str("%Y-%m-%dT%H:%M:%S.%f").unwrap();
+
+        // Write mandatory metadata
+        writeln!(writer, "CCSDS_TDM_VERS = 2.0").map_err(err_hdlr)?;
+        writeln!(
+            writer,
+            "\nCOMMENT Build by {} -- https://nyxspace.com",
+            prj_name_ver()
+        )
+        .map_err(err_hdlr)?;
+        writeln!(
+            writer,
+            "COMMENT Nyx Space provided under the AGPL v3 open source license -- https://nyxspace.com/pricing\n"
+        )
+        .map_err(err_hdlr)?;
+        writeln!(
+            writer,
+            "CREATION_DATE = {}",
+            Formatter::new(Epoch::now().unwrap(), iso8601_no_ts)
+        )
+        .map_err(err_hdlr)?;
+        writeln!(
+            writer,
+            "ORIGINATOR = {}\n",
+            metadata
+                .get("originator")
+                .unwrap_or(&"Nyx Space".to_string())
+        )
+        .map_err(err_hdlr)?;
+
+        // Create a new meta section for each tracker
+        // Get unique trackers and process each one separately
+        let trackers = self.unique_aliases();
+
+        for tracker in trackers {
+            let tracker_data = self.clone().filter_by_tracker(tracker.clone());
+
+            writeln!(writer, "META_START").map_err(err_hdlr)?;
+            writeln!(writer, "\tTIME_SYSTEM = UTC").map_err(err_hdlr)?;
+            writeln!(
+                writer,
+                "\tSTART_TIME = {}",
+                Formatter::new(tracker_data.start_epoch().unwrap(), iso8601_no_ts)
+            )
+            .map_err(err_hdlr)?;
+            writeln!(
+                writer,
+                "\tSTOP_TIME = {}",
+                Formatter::new(tracker_data.end_epoch().unwrap(), iso8601_no_ts)
+            )
+            .map_err(err_hdlr)?;
+
+            writeln!(
+                writer,
+                "\tPARTICIPANT_1 = {}",
+                if let Some(aliases) = &aliases {
+                    if let Some(alias) = aliases.get(&tracker) {
+                        alias
+                    } else {
+                        &tracker
+                    }
+                } else {
+                    &tracker
+                }
+            )
+            .map_err(err_hdlr)?;
+
+            writeln!(writer, "\tPARTICIPANT_2 = {spacecraft_name}").map_err(err_hdlr)?;
+
+            // Add additional metadata, could include timetag ref for example.
+            for (k, v) in &metadata {
+                if k != "originator" {
+                    writeln!(writer, "\t{k} = {v}").map_err(err_hdlr)?;
+                }
+            }
+
+            if self.unique_types().contains(&MeasurementType::Range) {
+                writeln!(writer, "\tRANGE_UNITS = km").map_err(err_hdlr)?;
+            }
+
+            if self.unique_types().contains(&MeasurementType::Azimuth)
+                || self.unique_types().contains(&MeasurementType::Elevation)
+            {
+                writeln!(writer, "\tANGLE_TYPE = AZEL").map_err(err_hdlr)?;
+            }
+
+            writeln!(writer, "META_STOP\n").map_err(err_hdlr)?;
+
+            // Write the data section
+            writeln!(writer, "DATA_START").map_err(err_hdlr)?;
+
+            // Process measurements for this tracker
+            for (epoch, measurement) in tracker_data.measurements {
+                for (mtype, value) in &measurement.data {
+                    let type_str = match mtype {
+                        MeasurementType::Range => "RANGE",
+                        MeasurementType::Doppler => "DOPPLER_INTEGRATED",
+                        MeasurementType::Azimuth => "ANGLE_1",
+                        MeasurementType::Elevation => "ANGLE_2",
+                    };
+
+                    writeln!(
+                        writer,
+                        "\t{:<20} = {:<23}\t{:.12}",
+                        type_str,
+                        Formatter::new(epoch, iso8601_no_ts),
+                        value
+                    )
+                    .map_err(err_hdlr)?;
+                }
+            }
+
+            writeln!(writer, "DATA_STOP\n").map_err(err_hdlr)?;
+        }
+
+        #[allow(clippy::writeln_empty_string)]
+        writeln!(writer, "").map_err(err_hdlr)?;
+
+        // Return the path this was written to
+        let tock_time = Epoch::now().unwrap() - tick;
+        info!("CCSDS TDM written to {} in {tock_time}", path_buf.display());
+        Ok(path_buf)
     }
 }
 
@@ -157,60 +317,4 @@ fn parse_measurement_line(
         })?;
 
     Ok(Some((mtype, epoch, value)))
-}
-
-#[cfg(test)]
-mod ut_tdm {
-    extern crate pretty_env_logger as pel;
-    use hifitime::TimeUnits;
-    use std::{collections::HashMap, path::PathBuf};
-
-    use crate::od::msr::TrackingDataArc;
-
-    #[test]
-    fn test_tdm_no_alias() {
-        let path: PathBuf = [
-            env!("CARGO_MANIFEST_DIR"),
-            "output_data",
-            "demo_tracking_arc.tdm",
-        ]
-        .iter()
-        .collect();
-
-        let tdm = TrackingDataArc::from_tdm(path, None).unwrap();
-        println!("{tdm}");
-    }
-
-    #[test]
-    fn test_tdm_with_alias() {
-        pel::init();
-        let mut aliases = HashMap::new();
-        aliases.insert("DSS-65 Madrid".to_string(), "DSN Madrid".to_string());
-
-        let path: PathBuf = [
-            env!("CARGO_MANIFEST_DIR"),
-            "output_data",
-            "demo_tracking_arc.tdm",
-        ]
-        .iter()
-        .collect();
-
-        let tdm = TrackingDataArc::from_tdm(path, Some(aliases)).unwrap();
-        println!("{tdm}");
-
-        let tdm_failed_downsample = tdm.clone().downsample(10.seconds());
-        assert_eq!(
-            tdm_failed_downsample.len(),
-            tdm.len(),
-            "downsampling should have failed because it's upsampling"
-        );
-
-        let tdm_downsample = tdm.clone().downsample(10.minutes());
-        println!("{tdm_downsample}");
-        assert_eq!(
-            tdm_downsample.len(),
-            tdm.len() / 10 + 1,
-            "downsampling has wrong sample count"
-        );
-    }
 }
