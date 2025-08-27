89fa7bcc50c9132cdb2356dd0b6ce844a878e78d
Chris <ChristopherRabotin@users.noreply.github.com>
2025-08-27 17:41:15 +0000

    Enforce max step when propagating backward

--- src/propagators/instance.rs
+++ src/propagators/instance.rs
@@ -478,8 +478,8 @@ where
                             * (self.prop.opts.tolerance / self.details.error)
                                 .powf(1.0 / f64::from(self.prop.method.order()));
 
-                        step_size_s = if proposed_step > self.prop.opts.max_step.to_seconds() {
-                            self.prop.opts.max_step.to_seconds()
+                        step_size_s = if proposed_step.abs() > self.prop.opts.max_step.to_seconds().abs() {
+                            self.prop.opts.max_step.to_seconds() * proposed_step.signum()
                         } else {
                             proposed_step
                         };
