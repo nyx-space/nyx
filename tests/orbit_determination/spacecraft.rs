a4bec78ecd5a4baca2eaada9fedcb1b29a6168d3
Christopher Rabotin <christopher.rabotin@gmail.com>
2025-11-21 22:16:16 -0700

    fix: threshold is between 0.0 and 1.0

--- tests/orbit_determination/spacecraft.rs
+++ tests/orbit_determination/spacecraft.rs
@@ -499,8 +499,8 @@ fn od_val_sc_srp_estimation_cov_test(
         "all residuals should be accepted"
     );
     assert!(
-        od_sol.residual_ratio_within_threshold(3.0).unwrap() > 80.0,
-        "expecting 80-90% of valid residual ratios"
+        od_sol.residual_ratio_within_threshold(3.0).unwrap() > 0.8,
+        "expecting >80-90% of valid residual ratios"
     );
     assert!(
         !od_sol.is_normal(None).unwrap(),
