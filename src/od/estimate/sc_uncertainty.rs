c521d31685d2a7d23e4e236ba04f5d84c3c6a99a
Christopher Rabotin <christopher.rabotin@gmail.com>
2026-01-21 21:39:29 -0700

    fix unit tests

--- src/od/estimate/sc_uncertainty.rs
+++ src/od/estimate/sc_uncertainty.rs
@@ -326,7 +326,7 @@ mod ut_sc_uncertainty {
         println!("{estimate}");
 
         let sma_sigma_km = estimate.sigma_for(StateParameter::SMA).unwrap();
-        assert!((sma_sigma_km - 1.3528088887049263).abs() < f64::EPSILON);
+        assert!((sma_sigma_km - 1.3528088887049263).abs() < 1e-8);
 
         let covar_keplerian = estimate.keplerian_covar();
         for i in 1..=3 {
