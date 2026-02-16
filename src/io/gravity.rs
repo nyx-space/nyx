e967a4ec78573ac85b1d8321556d14d8583076f6
Christopher Rabotin <christopher.rabotin@gmail.com>
2026-02-15 20:49:20 -0700

    Update EGM2008 to add the header
    SHADR files normally come with the header

--- src/io/gravity.rs
+++ src/io/gravity.rs
@@ -86,7 +86,7 @@ impl HarmonicsMem {
         gunzipped: bool,
     ) -> Result<HarmonicsMem, NyxError> {
         Self::load(
-            gunzipped, false, //SHADR has a header which we ignore
+            gunzipped, true, //SHADR has a header which we ignore
             degree, order, filepath,
         )
     }
