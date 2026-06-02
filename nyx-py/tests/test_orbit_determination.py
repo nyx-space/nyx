d5519fd49c8c7bcd0bb6bfecb293b91495ac5f4e
Christopher Rabotin <christopher.rabotin@gmail.com>
2026-06-02 14:00:20 +0200

    TODO: Add generating tracking data from python, then build/reload TDM

--- nyx-py/tests/test_orbit_determination.py
+++ nyx-py/tests/test_orbit_determination.py
@@ -10,17 +10,24 @@ from nyx_space.orbit_determination import (
 
 def test_ground_station():
     """
-    Demonstrate ground station config from a script, saving to a Dhall or Yaml, and reloading
+    Demonstrate ground station config from a script, saving to a Yaml, and reloading
     """
     gs = GroundStation(
         "Paris, FR",
         Location(2.3522, 48.8566, 0.4, FrameUid(399, 399), [], True),
-        [MeasurementType.Range, MeasurementType.Doppler],
+        [
+            MeasurementType.Range,
+            MeasurementType.Doppler,
+            MeasurementType.Elevation,
+            MeasurementType.Azimuth,
+        ],
     )
     as_yaml = gs.to_yaml()
     gs_reloaded = GroundStation.from_yaml(as_yaml)
     assert str(gs) == str(gs_reloaded)
     assert gs == gs_reloaded
+    print(gs)
+    print(repr(gs))
 
 
 def test_generate_tdm():
